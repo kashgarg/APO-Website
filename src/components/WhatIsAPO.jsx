56785402b288ec6a72ca51cd83351896ffcc978d
kashgarg <kashishgarg247@gmail.com>
2024-02-17 12:07:53 -0800

    Created WhatIsAPO component

---
+++ src/components/WhatIsAPO.jsx
@@ -0,0 +1,15 @@
+const WhatIsAPO = () => {
+    return (<>
+    <h1>What is APO</h1>
+    <img src="https://static1.squarespace.com/static/5e0ebeb743524e391e0c522c/t/5e1e38437abcac1fe3edacc3/1579038791636/apo.jpg?format=300w" alt="APO logo" />
+    <p className="text-lg">Alpha Phi Omega is an international co-educational service fraternity organized to provide 
+            community service, leadership development, and social opportunities for college students. 
+            It stands as the foremost collegiate fraternity in the United States, boasting chapters at
+            over 350 campuses, an active membership exceeding 25,000 students, and an alumni network
+            surpassing 500,000 members. Notably, there are 250 chapters in the Philippines, one in 
+            Australia, and one in Canada (Alpha Canada - our chapter!)</p>
+    </>
+    );
+}
+
+export default WhatIsAPO;
